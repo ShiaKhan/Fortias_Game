4ec01abf347bde244337f96c0780218a2fbd3aa6
QuanVo <quanvo472001@gmail.com>
2025-08-27 16:21:22 +0700

    +fixing scene ThiTran to Mockup
    -Done spawn player in 2 scene , need fix auto play

--- Assets/Scripts/PlayerControlSystem/CameraLayer/FollowCamera.cs
+++ Assets/Scripts/PlayerControlSystem/CameraLayer/FollowCamera.cs
@@ -2,11 +2,23 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+    public static FollowCamera instance;
     [SerializeField] private Transform _target;
     [SerializeField] private Vector3 _offset = new Vector3(0, 0, -8);
     [SerializeField] private float _smoothTime = 0.3f;
 
     private Vector3 _velocity = Vector3.zero;
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void LateUpdate()
     {
